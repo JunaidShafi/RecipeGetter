4d08e7755398fecf183a310f195d8ceeadc8525e
JunaidShafi <er.junaidshafi@gmail.com>
2024-09-23 10:28:48 +0530

    Upto Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Search from "./Components/Search.jsx";
+import { useState } from "react";
+import Recipe from "./Components/recipe.jsx";
+
 function App() {
-  return <>Hey Recipe app</>;
+  const [foodData, setFoodsData] = useState([]);
+  return (
+    <div>
+      <Search foodData={foodData} setFoodsData={setFoodsData} />
+      <Recipe foodData={foodData} setFoodsData={setFoodsData} />
+    </div>
+  );
 }
 
 export default App;
